dd1c49d42b2ba77f12ed18579970de4da808cca7
Lilsherlock <potatogames15@gmail.com>
2020-09-17 00:44:49 -0500

    update

--- monty.c
+++ monty.c
@@ -2,36 +2,34 @@
 
 int main(int argc, char **argv)
 {
-    char *filename = argv[1];
-    char *line = NULL, *token = NULL, *value = NULL;
-    size_t len = 0;
-    unsigned int line_number = 0;
-    ssize_t line_size;
-    FILE *fp;
+	char *filename = argv[1];
+	char *line = NULL, *opcode = NULL, *value = NULL;
+	size_t len = 0;
+	ssize_t line_size;
+	stack_t *stack = NULL;
+	unsigned int line_number = 0;
+	FILE *fp;
 
-    fp = fopen(filename, "r");
-    if (!filename)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        return (EXIT_FAILURE);
-    }
-    if (!fp)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
-        return (EXIT_FAILURE);
-    }
-
-    line_size = getline(&line, &len, fp);
-    while (line_size >= 0)
-    {
-        line_number++;
-        token = strtok(line, DELIMITERS);
-        value = strtok(NULL, DELIMITERS);
-        printf("%s %s\n", line, value);
-        line_size = getline(&line, &len, fp);
-    }
-    free(line);
-    line = NULL;
-    fclose(fp);
-    return (0);
+	fp = fopen(filename, "r");
+	if (!filename)
+	{
+		fprintf(stderr, "USAGE: mchar *line_numberonty file\n");
+		return (EXIT_FAILURE);
+	}
+	if (!fp)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		return (EXIT_FAILURE);
+	}
+	line_size = getline(&line, &len, fp);
+	while (line_size >= 0)
+	{
+		line_number++;
+		verify(line, line_number, &stack);
+		line_size = getline(&line, &len, fp);
+	}
+	free(line);
+	line = NULL;
+	fclose(fp);
+	return (EXIT_SUCCESS);
 }
--- monty.h
+++ monty.h
@@ -21,9 +21,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 /**
  * struct instruction_s - opcode and its function
@@ -35,7 +35,13 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+void verify(char *line, unsigned int line_number, stack_t **stack);
+int opcode_selector(char *token, stack_t **stack, unsigned int line_number);
+
+void push(unsigned int line_number, stack_t **stack, char *token_value);
+void pall(stack_t **stack, unsigned int line_number);
 #endif
