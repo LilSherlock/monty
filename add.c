c7d4447dcacd4c025e64b3d988c67dbefc0a0007
Lilsherlock <potatogames15@gmail.com>
2020-09-17 16:14:15 -0500

    update

---
+++ add.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * add - add the two last elements
+ * @stack: stack
+ * @line_number: - number of line
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	int a = 0;
+	int b = 0;
+	int temp = 0;
+
+	if (stack_length(stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	a = (*stack)->n;
+	b = (*stack)->next->n;
+
+	temp = a + b;
+	pop(stack, line_number);
+	(*stack)->n = temp;
+}
